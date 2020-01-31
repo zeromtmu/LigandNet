8e358cec8994fb6c1c95584ed84679cab5a8df2c
hassanmohsin <mhassan@miners.utep.edu>
2020-01-31 09:44:36 -0700

    Add prediction for drug central

--- prediction/drug_central/predict.py
+++ prediction/drug_central/predict.py
@@ -1,70 +1,43 @@
-
-# coding: utf-8
-
-# In[15]:
-
-
-import pandas as pd
+import numpy as np
+from sklearn.externals import joblib
+import os
+import json
+MODELS_DIR = "../../uniprot_models"
 import sys
 sys.path.append("../../../ddt/")
 from utility import FeatureGenerator
-from sklearn.externals import joblib
-import os
-MODELS_DIR = "../../models"
 PY_VERSION = '27' if sys.version_info[0] < 3 else '35'
-import multiprocessing as mp
-import json
-import warnings
-warnings.filterwarnings("ignore")
 
+# Extract features
+#ft = FeatureGenerator()
+#ft.load_sdf("structures.molV2.sdf")
+#compound_list, features = ft.extract_tpatf()
 
-# In[16]:
-
-
-samples_df = pd.read_csv("sellect_drug_library.csv")
-# samples_df.head()
-
-
-# In[14]:
-
+features = np.load('drug_central.npy')
+compound_list = open('compound_list.txt').readlines()[0].split(',')
 
 def get_models():    
-    model_names = open('../../py' + PY_VERSION + '_models.txt', 'r').readlines()[0].split(',')
+    model_names = open('../../py' + PY_VERSION + '_uniprot_models.txt', 'r').readlines()[0].split(',')
     for model in model_names:
         yield model, joblib.load(os.path.join(MODELS_DIR, model))
+        
 
-
-# In[8]:
-
-
-def get_prediction(smiles, confidence=0.9):
-    actives = []
-    ft = FeatureGenerator()
-    ft.load_smiles(smiles)
-    features = ft.extract_tpatf()
+def get_prediction(confidence=0.9):
+    results = {}
     for model_name, model in get_models():
+        print("Predicting using {}".format(model_name))
         if type(model).__name__ == "SVC":
             pred = model.predict(features)
-            if pred[0] == 1.0: actives.append(model_name)
+            mask = np.where(pred)
+            results[model_name] = np.array(compound_list)[mask].tolist()
         else:
             pred = model.predict_proba(features)
-            if pred[0][1] > confidence: actives.append(model_name)
-    return smiles, actives
-
-
-# In[10]:
-
-
-pool = mp.Pool(mp.cpu_count())
-results = [pool.apply_async(get_prediction, args=(smiles,)) for smiles in samples_df.SMILES.tolist()]
-output = [p.get() for p in results]
-
-
-# In[24]:
-
-
-output_dict = {}
-for _smiles, _actives in output:
-    output_dict[_smiles] =_actives
-json.dump(output_dict, open('py' + PY_VERSION + '_results.json', 'w'))
+            mask = np.where(pred[:, 1] > confidence)
+            c_list = np.array(compound_list)[mask].tolist()
+            results[model_name] = [(c, str(f)) for c, f in zip(c_list, pred[:, 1][mask])]
+    return results 
+    
+if __name__ == "__main__":
+    results_dict = get_prediction()
+    json.dump(results_dict, open('py' + PY_VERSION + '_results.json', 'w'))
 
